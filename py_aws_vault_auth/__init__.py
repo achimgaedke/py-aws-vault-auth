37f26c5478a237756bb0809a236f258daebc1560
Achim Gädke <achimgaedke@users.noreply.github.com>
2023-04-03 11:34:54 +1200

    use pty for communication with aws-vault

--- py_aws_vault_auth/__init__.py
+++ py_aws_vault_auth/__init__.py
@@ -1,3 +1,10 @@
-from .auth import authenticate, to_boto_auth, to_s3fs_auth, to_environ_auth
+from .auth import (authenticate, expiration_time, to_boto_auth,
+                   to_environ_auth, to_s3fs_auth)
 
-__all__ = ["authenticate", "to_boto_auth", "to_s3fs_auth", "to_environ_auth"]
+__all__ = [
+    "authenticate",
+    "expiration_time",
+    "to_boto_auth",
+    "to_environ_auth",
+    "to_s3fs_auth",
+]
--- py_aws_vault_auth/auth.py
+++ py_aws_vault_auth/auth.py
@@ -1,29 +1,29 @@
 import datetime
 import json
 import os
-import subprocess
+import pty
 import sys
 import warnings
 
-from .utils import non_block_read, CHAR_CODE
 from .prompts import input_prompt
-
+from .utils import CHAR_CODE, non_block_read
 
 AWS_VAULT_CMD = "aws-vault"
 """
 The default ``aws-vault`` command.
 """
 
-AWS_ENV_VARS = ["AWS_ACCESS_KEY_ID",
-                "AWS_SECRET_ACCESS_KEY",
-                "AWS_SESSION_TOKEN",
-                "AWS_SECURITY_TOKEN",
-                "AWS_REGION",
-                "AWS_DEFAULT_REGION",
-                "AWS_VAULT",
-                "AWS_SESSION_EXPIRATION",
-                "AWS_CREDENTIAL_EXPIRATION",
-                ]
+AWS_ENV_VARS = [
+    "AWS_ACCESS_KEY_ID",
+    "AWS_SECRET_ACCESS_KEY",
+    "AWS_SESSION_TOKEN",
+    "AWS_SECURITY_TOKEN",
+    "AWS_REGION",
+    "AWS_DEFAULT_REGION",
+    "AWS_VAULT",
+    "AWS_SESSION_EXPIRATION",
+    "AWS_CREDENTIAL_EXPIRATION",
+]
 """
 Names of environment variables expected to be returned from ``aws-vault``
 """
@@ -31,7 +31,11 @@ Names of environment variables expected to be returned from ``aws-vault``
 AWS_VAULT_EXEC_PROCESS = [
     sys.executable,
     "-c",
-    "import json, os, sys; json.dump({k: v for k, v in os.environ.items() if k.startswith('AWS_')}, sys.stdout)",
+    (
+        "import json, os, sys; json.dump("
+        "{k: v for k, v in os.environ.items() if k.startswith('AWS_')}"
+        ", sys.stdout)"
+    ),
 ]
 
 
@@ -44,7 +48,7 @@ def expiration_time(aws_vault_credentials, timezone=None):
     Return the credentials expiration time mapped to the (local) timezone
 
     Requires either python>=3.11 or the ``dateutil`` package.
-    
+
     :param aws_vault_credentials: credentials returned from ``authenticate``
     :type aws_vault_credentials: dict[str,str]
     :param timezone: Timezone to map the expiration time to, defaults
@@ -54,11 +58,19 @@ def expiration_time(aws_vault_credentials, timezone=None):
     :rtype: datetime.datetime
     """
     try:
-        return datetime.datetime.fromisoformat(aws_vault_credentials["AWS_SESSION_EXPIRATION"]).astimezone(timezone)
+        return datetime.datetime.fromisoformat(
+            aws_vault_credentials["AWS_SESSION_EXPIRATION"]
+        ).astimezone(timezone)
     except ValueError:
         pass
 
-    import dateutil.parser
+    try:
+        import dateutil.parser
+    except ImportError:
+        warnings.warn(
+            "please install the dateutil package or upgrade to python>=3.11"
+        )
+        raise
     return dateutil.parser.isoparse(expiration_time).astimezone(timezone)
 
 
@@ -69,7 +81,8 @@ def to_boto_auth(aws_vault_credentials):
     """
     return {
         "aws_access_key_id": aws_vault_credentials["AWS_ACCESS_KEY_ID"],
-        "aws_secret_access_key": aws_vault_credentials["AWS_SECRET_ACCESS_KEY"],
+        "aws_secret_access_key":
+            aws_vault_credentials["AWS_SECRET_ACCESS_KEY"],
         "aws_session_token": aws_vault_credentials["AWS_SESSION_TOKEN"],
         "region_name": aws_vault_credentials["AWS_REGION"],
     }
@@ -113,18 +126,18 @@ def authenticate(profile,
 
     The MFA token will only be requested when necessary.
 
-    With the (default) prompt `python`, the MFA token is be requested via
-    the python `input` command. That allows the token to be entered inside
+    With the (default) prompt ``python``, the MFA token is be requested via
+    the python ``input`` command. That allows the token to be entered inside
     a jupyter notebook context.
 
     This function adds the prompt method ``python`` to loop any dialgoues
     through and uses the `terminal` under the hood.
 
-    The authentication is returned as dictionary (i.e. not set as environment
-    variables) so multiple profiles can be used easily within one python
-    process.
+    The credentials are returned as dictionary (i.e. not immediately set as
+    environment variables) so multiple profiles can be used easily within one
+    python process.
 
-    Use `return_as` to get parameters immediately usable with boto, pandas.
+    Use ``return_as`` to get parameters immediately usable with boto, pandas.
 
     Alternatively do not specify `return_as` and use the functions
     ``to_environ_auth``, ``to_boto_auth`` and ``to_s3fs_auth`` to convert the
@@ -153,7 +166,7 @@ def authenticate(profile,
     :returns: the authentication credentials
     :rtype: dict[str,str]
 
-    :raises: subprocess.CalledProcessError
+    :raises: Exception
     """
 
     if aws_vault_cmd is None:
@@ -199,81 +212,105 @@ def authenticate(profile,
                 f"--{k.replace('_', '-')}",
                 v])
 
-    aws_vault_process = subprocess.Popen(
-        [aws_vault_cmd, "exec",
-         profile,
-         *aws_vault_args,
-         "--",
-         *AWS_VAULT_EXEC_PROCESS,
-         ],
-        env=aws_vault_env,
-        stdin=subprocess.PIPE,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        bufsize=0,
-    )
+    (pid, fd) = pty.fork()
 
-    stderr_data = ""
+    if pid == 0:
+        # child process to start aws-vault
+        os.closerange(3, 1 << 15)
+        # TODO switch off input echoing
 
-    if prompt_function is not None:
-        # use threaded read, emulating non-blocking read
-        # internally this seems to be a busy wait loop... sorry!
-        stderr_stream_read = non_block_read(aws_vault_process.stderr)
-        stderr_stream_read.start()
-
-        while stderr_stream_read.is_alive():
-            stderr_stream_read.join(0.05)
-            stderr_lines = stderr_stream_read.get_completed_lines()
-            if stderr_lines:
-                stderr_data += stderr_lines
-                stderr_message(stderr_lines)
-            if (stderr_stream_read.is_alive() and
-                    stderr_stream_read.read_buffer):
-                # incomplete line, probably waiting for a user input
-                # expect buffer being written at once, but give it a chance
-                stderr_stream_read.join(0.01)
-                stderr_lines = stderr_stream_read.get_completed_lines()
-                if stderr_lines:
-                    stderr_data += stderr_lines
-                    stderr_message(stderr_lines)
-                if stderr_stream_read.read_buffer.startswith(b"Enter "):
-                    # now show a prompt & get the result
-                    prompt_text = stderr_stream_read.get_text()
-                    response = prompt_function(prompt_text)
-                    aws_vault_process.stdin.write(
-                        (response+"\n").encode(encoding=CHAR_CODE))
-
-        stderr_stream_read.join()
-        stderr_lines = stderr_stream_read.get_text()
-        if stderr_lines:
-            stderr_data += stderr_lines
-            stderr_message(stderr_lines)
-
-    # internally this seems to be a another busy wait loop... sorry!
-    aws_vault_process.wait()
-
-    stderr_lines = aws_vault_process.stderr.read().decode(encoding=CHAR_CODE)
-    stderr_data += stderr_lines
-    stderr_message(stderr_lines)
-    stdout_data = aws_vault_process.stdout.read().decode(encoding=CHAR_CODE)
-
-    if aws_vault_process.returncode != 0:
-        raise subprocess.CalledProcessError(
-            cmd=aws_vault_process.args,
-            returncode=aws_vault_process.returncode,
-            stderr=stderr_data,
-            output=stdout_data,
+        os.execvpe(
+            aws_vault_cmd,
+            [os.path.basename(aws_vault_cmd),
+             "exec",
+             profile,
+             *aws_vault_args,
+             "--",
+             *AWS_VAULT_EXEC_PROCESS,
+             ],
+            aws_vault_env
         )
-    aws_vault_credentials = {k: v for k, v in json.loads(stdout_data).items()
-                             if k in AWS_ENV_VARS}
+        # (maybe throws an exception) never returns from here
 
-    if return_as in ["boto", "boto3"]:
-        return to_boto_auth(aws_vault_credentials)
-    elif return_as == "s3fs":
-        return to_s3fs_auth(aws_vault_credentials)
-    elif return_as in ["env", "environ"]:
-        return to_environ_auth(aws_vault_credentials)
-    elif return_as in [None, ""]:
-        return aws_vault_credentials
+    # that's the parent process
 
-    raise ValueError(f"unkonw return type {return_as}")
+    aws_vault_read = non_block_read(open(fd, "rb"))
+    aws_vault_read.start()
+
+    # collect all output data except from input prompts
+    aws_vault_output_data = ""
+
+    try:
+        if prompt_function is not None:
+            # start reading and waiting for an input prompt
+
+            # TODO: cleanup, this loop gets a bit unweildy
+            while aws_vault_read.is_alive():
+                aws_vault_read.join(0.05)
+                stderr_lines = aws_vault_read.get_completed_lines()
+                if stderr_lines:
+                    aws_vault_output_data += stderr_lines
+                    stderr_message(stderr_lines)
+                if (aws_vault_read.is_alive() and
+                        aws_vault_read.read_buffer):
+                    # incomplete line, probably waiting for a user input
+                    # expect buffer being written at once, but give it a chance
+                    aws_vault_read.join(0.01)
+                    stderr_lines = aws_vault_read.get_completed_lines()
+                    if stderr_lines:
+                        aws_vault_output_data += stderr_lines
+                        stderr_message(stderr_lines)
+                    if aws_vault_read.read_buffer.startswith(b"Enter "):
+                        # now show a prompt & get the result
+                        prompt_text = aws_vault_read.get_text()
+                        response = prompt_function(prompt_text)
+                        os.write(
+                            fd,
+                            # the pty uses \r\n for "enter"
+                            response.encode(encoding=CHAR_CODE) + b"\r\n")
+                        # now read echoed input and discard it
+                        aws_vault_read.join(0.01)
+                        stderr_lines = aws_vault_read.get_completed_lines()
+                        for line in stderr_lines.splitlines(keepends=True):
+                            if line.strip() != response:
+                                stderr_message(line)
+                                stderr_lines += line
+    finally:
+        # make sure resources are freed
+        aws_vault_read.join()
+        os.close(fd)
+        aws_vault_status = os.waitpid(pid, 0)
+
+    # read remaining completed lines
+    stderr_lines = aws_vault_read.get_completed_lines()
+    aws_vault_output_data += stderr_lines
+    stderr_message(stderr_lines)
+
+    # last incomplete line, might contain the credentials
+    remaining_data = aws_vault_read.read_buffer.decode(CHAR_CODE)
+
+    if os.waitstatus_to_exitcode(aws_vault_status[1]) == 0:
+        try:
+            aws_vault_credentials = json.loads(remaining_data)
+        except json.decoder.JSONDecodeError:
+            stderr_message(remaining_data)
+            raise Exception(aws_vault_output_data + remaining_data)
+
+        aws_vault_credentials = {
+            k: v for k, v in aws_vault_credentials.items()
+            if k in AWS_ENV_VARS
+        }
+
+        if return_as in ["boto", "boto3"]:
+            return to_boto_auth(aws_vault_credentials)
+        elif return_as == "s3fs":
+            return to_s3fs_auth(aws_vault_credentials)
+        elif return_as in ["env", "environ"]:
+            return to_environ_auth(aws_vault_credentials)
+        elif return_as in [None, ""]:
+            return aws_vault_credentials
+
+        raise ValueError(f"unkonw return type {return_as}")
+
+    stderr_message(remaining_data)
+    raise Exception(aws_vault_output_data + remaining_data)
